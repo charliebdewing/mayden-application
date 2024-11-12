7fced26cd24f5b53378eae405182bb6d39ee3efa
charliebdewing <charlie@bror.co.uk>
2024-11-12 13:39:31 +0000

    Improved styling of header logo when logged in

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,27 +1,42 @@
 <template>
   <Toolbar v-if="isAuthenticated"></Toolbar>
-
   <v-container
     class="main-container fill-height d-flex flex-column align-center justify-center"
     max-width="900"
     width="100%"
   >
-    <v-img
-      class="mb-4 flex-1-1"
-      max-height="150"
-      height="150"
-      width="200px"
-      src="@/assets/logo.svg"
-    />
+    <template v-if="!isAuthenticated">
+      <v-img
+        class="mb-4 flex-1-1"
+        max-height="150"
+        height="150"
+        width="200px"
+        src="@/assets/logo.svg"
+      />
 
-    <div class="text-center mb-8">
-      <h1 class="text-h2 font-weight-bold">
-        body<span :style="{ color: '#d64405' }">IO</span>
-      </h1>
-    </div>
+      <div class="text-center mb-8">
+        <h1 class="text-h2 font-weight-bold">
+          body<span :style="{ color: '#d64405' }">IO</span>
+        </h1>
+      </div>
 
-    <Login v-if="!isAuthenticated" />
-    <Shopping-List v-else />
+      <Login />
+    </template>
+    <template v-else>
+      <div class="text-center mb-8 d-flex align-center">
+        <v-img
+          class="mb-4 me-4"
+          height="80"
+          width="80"
+          src="@/assets/logo.svg"
+        />
+        <h1 class="text-h2 font-weight-bold">
+          body<span :style="{ color: '#d64405' }">IO</span>
+        </h1>
+      </div>
+
+      <Shopping-List />
+    </template>
   </v-container>
 </template>
 
@@ -31,6 +46,4 @@ import { useAuthStore } from "@/stores/user";
 
 const authStore = useAuthStore();
 const isAuthenticated = computed(() => authStore.isAuthenticated);
-
-console.log(isAuthenticated.value);
 </script>
